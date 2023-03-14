45ee9f14ba3183eb584340e4a711da24078e0d29
Germain <germain.leignel@gmail.com>
2023-03-14 09:41:59 +0100

    chatgpt

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ import discord
 
 load_dotenv()
 
-activity = discord.Activity(type=discord.ActivityType.watching, name="Reply with !r to a message",)
+activity = discord.Activity(
+    type=discord.ActivityType.watching, name="Reply with !r to a message",)
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents, activity=activity, status=discord.Status.idle)
+bot = commands.Bot(command_prefix='!', intents=intents,
+                   activity=activity, status=discord.Status.idle)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 DISCORD_KEY = os.getenv("DISCORD_KEY")
+
 
 def generate_sarcastic_response(prompt):
     response = openai.Completion.create(
@@ -27,6 +30,20 @@ def generate_sarcastic_response(prompt):
     return response["choices"][0]["text"].strip()
 
 
+def generate_chatgpt_response(prompt):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a program that generates jokes and sarcastic responses"},
+            {"role": "user", "content": prompt},
+        ]
+    )
+    
+    print(response)
+
+    return response["choices"][0]["message"]["content"].strip()
+
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
@@ -35,8 +52,8 @@ async def on_ready():
 @bot.command()
 async def r(ctx):
     message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
-    
-    response = generate_sarcastic_response(message.content)
+
+    response = generate_chatgpt_response(message.content)
 
     await ctx.send(response)
 
